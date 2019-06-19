26c2620fb4f12bc3037233d228eba192c9830bf6
Fly <3713548+flyinghuman@users.noreply.github.com>
2019-06-19 13:17:10 +0200

    fix naming conflict with existing Makefile scripts

--- .travis/test_06_script_b.sh
+++ .travis/test_06_script_b.sh
@@ -9,8 +9,9 @@ export LC_ALL=C.UTF-8
 #deploy test builds
 if [ "$DEPLOY_TEST_BUILDS" = "true" ] && [ "$BUILD_ONLY_DEPENDS" = "false" ]; then
   BEGIN_FOLD deploybins
-  if [ "$REASON" = "MacOS" ]; then DOCKER_EXEC pwd && find . -name '*.dmg' && echo $OUTDIR && mkdir -p "$TRAVIS_BUILD_DIR/release" && cp *.dmg "$TRAVIS_BUILD_DIR/release/UCC-$REASON-$TRAVIS_BRANCH.dmg" && cd "$TRAVIS_BUILD_DIR/release" && ls; fi
-  if [ "$REASON" != "MacOS" ]; then DOCKER_EXEC pwd && mkdir -p "$TRAVIS_BUILD_DIR/release/UCC-$REASON-$TRAVIS_BRANCH" && find $OUTDIR && cd $OUTDIR && pwd && ls -R && cp -a bin/* "$TRAVIS_BUILD_DIR/release/UCC-$REASON-$TRAVIS_BRANCH/" && cd "$TRAVIS_BUILD_DIR/release" && strip UCC-$REASON-$TRAVIS_BRANCH/* && ls UCC-$REASON-$TRAVIS_BRANCH/* && zip -r UCC-$REASON-$TRAVIS_BRANCH.zip * && ls; fi
+  if [ "$REASON" = "MacOS" ]; then DOCKER_EXEC pwd && find . -name '*.dmg' && echo $OUTDIR && mkdir -p "$TRAVIS_BUILD_DIR/uccrelease" && cp *.dmg "$TRAVIS_BUILD_DIR/uccrelease/UCC-$REASON-$TRAVIS_BRANCH.dmg" && cd "$TRAVIS_BUILD_DIR/uccrelease" && ls; fi
+  if [ "$REASON" != "MacOS" ]; then DOCKER_EXEC pwd && mkdir -p "$TRAVIS_BUILD_DIR/uccrelease/UCC-$REASON-$TRAVIS_BRANCH" && find $OUTDIR && cd $OUTDIR && pwd && ls -Rha && cp -a bin/* "$TRAVIS_BUILD_DIR/uccrelease/UCC-$REASON-$TRAVIS_BRANCH/" && cd "$TRAVIS_BUILD_DIR/uccrelease" && strip UCC-$REASON-$TRAVIS_BRANCH/* && ls UCC-$REASON-$TRAVIS_BRANCH/* && zip -r UCC-$REASON-$TRAVIS_BRANCH.zip * && ls; fi
+  if [ "$ADDWINSETUP" = "true" ]; then DOCKER_EXEC cd "$TRAVIS_BUILD_DIR/release" && pwd && ls -R && cp -a *.exe "$TRAVIS_BUILD_DIR/uccrelease/UCC-$REASON-$TRAVIS_BRANCH/" && cd "$TRAVIS_BUILD_DIR/uccrelease" && strip *.exe && ls -Rha && zip -r UCC-$REASON-$TRAVIS_BRANCH.zip * && ls; fi
   DOCKER_EXEC git init
   DOCKER_EXEC git config --global user.email "3713548+flyinghuman@users.noreply.github.com"
   DOCKER_EXEC git config --global user.name "Travis-User"
